7ad6bd402a579e51b3af567912b237df85f36ff8
allthingscandid@gmail.com <allthingscandid@gmail.com@8660b971-0f8b-ea4a-1521-bd2fb6e1095f>
2011-03-18 03:17:17 +0000

    version 4, updated todo.xlsx, added KEYS.txt, camera now uses zoom in/out preset positions to interpolate between with gui button (top-left)

--- trunk/see-saw-unity/Scripts/SmoothLookAtFollow.cs
+++ trunk/see-saw-unity/Scripts/SmoothLookAtFollow.cs
@@ -3,90 +3,102 @@ using System.Collections;
 
 public class SmoothLookAtFollow : MonoBehaviour
 {
-    public Transform target;
+    GameObject m_ZoomInOutBtn;
+    Game m_Game;
 
-    Vector3 m_vCurrLookAt;
-    Vector3 m_vOrigPos;
-    Quaternion m_vOrigRot;
+    public Transform LookAtTarget;
 
-    public float LookAtDamping = 8.0f;
+    Vector3 m_vZoomedInPos;
+    Vector3 m_vZoomedOutPos;
+    Vector3 m_vZoomedInLA;
+
+    public float LookAtDampingReset = 1.0f;
+    public float LookAtDampingPlay = 8.0f;
     public float FollowDamping = 6.0f;
     public float MoveCamSpeed = 5.0f;
-    public float MaxZDist = 2.0f;
 
-    float m_fCurrZDist;
+    public float ZoomedInZPosOffset = 25.0f;
+    public float ZoomedInYPosOffset = 10.0f;
+    public float ZoomedInYLAOffset = 10.0f;
 
-    public bool smooth = true;
+    public float ZoomedOutYPosOffset = 15.0f;
+    //public float ZoomedOutYLAOffset = 10.0f;
+
+    bool m_bZoomedIn = true;
+    bool m_bResetting = false;
 
     void LateUpdate()
     {
-        if (target)
+        if (Game.g_bWeightDropped)
         {
-            if (Game.g_bWeightDropped)
-            {
-	            if (smooth)
-	            {
-	                // Look at and dampen the rotation
-	                Quaternion rotation = Quaternion.LookRotation(target.position - transform.position);
-	                transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * LookAtDamping);
-	                
-	                transform.position = Vector3.Lerp(transform.position, new Vector3(target.position.x, transform.position.y, transform.position.z), Time.deltaTime * FollowDamping);
-	            }
-	            else
-	            {
-	                // Just lookat
-	                transform.LookAt(target);
-	            }
-            }
-            else
-            {
-                // "pan"
-                if (Input.GetButton("Pan Left"))
+            // Look at and dampen the rotation
+            Quaternion rotation = Quaternion.LookRotation(LookAtTarget.position - transform.position);
+            transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * LookAtDampingPlay);
+            
+            transform.position = Vector3.Lerp(transform.position, new Vector3(LookAtTarget.position.x, transform.position.y, transform.position.z), Time.deltaTime * FollowDamping);
+        }
+        else
+        {
+            if (Input.GetMouseButtonUp(0))
+                if (m_ZoomInOutBtn.guiTexture.HitTest(Input.mousePosition))
                 {
-                    m_vCurrLookAt -= Vector3.right * MoveCamSpeed * Time.deltaTime;
-                    transform.position -= Vector3.right * MoveCamSpeed * Time.deltaTime;
-                }
-                else if (Input.GetButton("Pan Right"))
-                {
-                    m_vCurrLookAt += Vector3.right * MoveCamSpeed * Time.deltaTime;
-                    transform.position += Vector3.right * MoveCamSpeed * Time.deltaTime;
-                }
-                // "zoom"
-                if (Input.GetButton("Zoom Out") && m_fCurrZDist > -MaxZDist)
-                {
-                    m_fCurrZDist -= MoveCamSpeed * Time.deltaTime;
-                    transform.position -= Vector3.forward * MoveCamSpeed * Time.deltaTime;
-                }
-                else if (Input.GetButton("Zoom In") && m_fCurrZDist < MaxZDist)
-                {
-                    m_fCurrZDist += MoveCamSpeed * Time.deltaTime;
-                    transform.position += Vector3.forward * MoveCamSpeed * Time.deltaTime;
+                    m_bZoomedIn = !m_bZoomedIn;
+                    m_bResetting = false;
                 }
 
-                transform.LookAt(m_vCurrLookAt);
+            if (m_bZoomedIn)    // zoomed in or currently zooming in
+            {
+                // Look at and dampen the rotation
+                if (m_bResetting)
+                {
+                    Quaternion rotation = Quaternion.LookRotation(m_vZoomedInLA - transform.position);
+                    transform.rotation = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * LookAtDampingReset);
+                }
+                transform.position = Vector3.Lerp(transform.position, m_vZoomedInPos, Time.deltaTime * MoveCamSpeed);
+            } 
+            else   // zoomed out or currently zooming out
+            {
+                transform.position = Vector3.Lerp(transform.position, m_vZoomedOutPos, Time.deltaTime * MoveCamSpeed);
             }
         }
     }
 
     void Start()
     {
+        m_ZoomInOutBtn = GameObject.Find("ZoomInOutButton");
+        m_Game = GameObject.Find("Game").GetComponent<Game>();
+
         // Make the rigid body not change rotation
         if (rigidbody)
             rigidbody.freezeRotation = true;
 
-        m_vOrigRot = new Quaternion(0,0,0,1);
-        m_vOrigPos = transform.position;
-        m_vCurrLookAt = target.position;
+        // setup the 2 modes' transforms:
+        Vector3 wedgePos = GameObject.Find("Wedge").GetComponent<Wedge>().transform.position;
+        m_vZoomedInPos = wedgePos;
+        m_vZoomedInPos += Vector3.forward * -ZoomedInZPosOffset;
+        m_vZoomedInPos += Vector3.up * ZoomedInYPosOffset;
+        m_vZoomedInLA = wedgePos + Vector3.up * ZoomedInYLAOffset;
+
+        // TODO:: do the calculation to have the whole level in view
+        //        or if it's too large, just a certain section..
+        m_vZoomedOutPos = m_Game.GetLevelCenterLookAt() + Vector3.forward * -50.0f;
+        m_vZoomedOutPos = new Vector3(m_vZoomedOutPos.x, m_vZoomedInPos.y, m_vZoomedOutPos.z);
+
+        // start zoomed in?
+        // TODO:: probably set up a start for each level
+        //        to be zoomed out at first, wait a bit, 
+        //        then zoom in
+        transform.position = m_vZoomedInPos;
+        transform.LookAt(m_vZoomedInLA);
     }
     
     public void OnReset()
     {
         // TODO:: smooth interp to final position and look at on reset:
-
-        m_fCurrZDist = 0.0f;
-
-        transform.rotation = m_vOrigRot;
-        transform.position = m_vOrigPos;
-        m_vCurrLookAt = target.position;
+        m_bResetting = true;
+        m_bZoomedIn = true;
+//         Vector3 wedgePos = GameObject.Find("Wedge").GetComponent<Wedge>().transform.position;
+//         transform.position = m_vZoomedInPos;
+//        transform.LookAt(wedgePos + Vector3.up * ZoomedInYLAOffset);
     }
 }
