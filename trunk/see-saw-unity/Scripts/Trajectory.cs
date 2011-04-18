7358270e88e4f203c6e160b77cd85764adaf4361
allthingscandid@gmail.com <allthingscandid@gmail.com@8660b971-0f8b-ea4a-1521-bd2fb6e1095f>
2011-04-18 06:25:42 +0000

    Trajectory

--- trunk/see-saw-unity/Scripts/Trajectory.cs
+++ trunk/see-saw-unity/Scripts/Trajectory.cs
@@ -10,14 +10,13 @@ public class Trajectory : MonoBehaviour
     public GameObject TrajObj;
 
     /// <summary>
-    /// x = vt * cos(theta)
-    /// y = vt * sin(theta) - (0.5 * g * t^2)
+    /// x = vt
+    /// y = vt - (0.5 * g * t^2)
     /// </summary>
 
     float m_fGravity;
-    float m_fAngle;
 
-    Vector3 m_vVelocity;
+    public float m_fVelX, m_fVelY;
 
     GameObject[] m_Trajectory;
 
@@ -25,49 +24,93 @@ public class Trajectory : MonoBehaviour
 
     void Start()
     {
-        Debug.Log("Traj Start");
-
         m_fGravity = -Physics.gravity.y;
-        Debug.Log("Grav:" + m_fGravity.ToString());
-        m_fAngle = 45.0f;
         m_Player = GameObject.Find("Player");
-        m_vVelocity = new Vector3(50.0f, 50.0f, 0.0f);
         m_Trajectory = new GameObject[NumSamples];
         for (int i = 0; i < NumSamples; ++i)
             m_Trajectory[i] = (GameObject)Instantiate(TrajObj);
+
+        m_fVelY = _CalculateVelY();
+        m_fVelX = _CalculateVelX();
     }
 
     void Update()
     {
-        OnWedgeMoved();
-        OnWeightMoved();
-
-        float x, y, t;
-        for (int i = 0; i < NumSamples; ++i)
+        if (!Game.Instance.WeightDropped)
         {
-            t = (((float)i+1) * TimeInterval);
-            x = m_vVelocity.x * t * Mathf.Cos(m_fAngle);
-            y = m_vVelocity.y * t * Mathf.Sin(m_fAngle) - (0.5f * m_fGravity * t * t);
-            m_Trajectory[i].transform.position = new Vector3(x, y, 0.0f);
-            m_Trajectory[i].transform.position += m_Player.transform.position;
+            m_fVelX = _CalculateVelX();
+            m_fVelY = _CalculateVelY();
+
+            float x, y, t;
+	        for (int i = 0; i < NumSamples; ++i)
+	        {
+	            t = (((float)i+1) * TimeInterval);
+                x = m_fVelX * t;
+                y = m_fVelY * t - (0.5f * m_fGravity * t * t);
+	            m_Trajectory[i].transform.position = new Vector3(x, y, 0.0f);
+	            m_Trajectory[i].transform.position += m_Player.transform.position;
+	        }
         }
     }
 
-    public void OnWeightMoved()
+    private float _CalculateVelY()
     {
-        // Calculate the velocity
-        if (Input.GetButton("Move Weight Up"))
-            m_vVelocity += new Vector3(10.0f, 10.0f, 0.0f) * Time.deltaTime;
-        else if (Input.GetButton("Move Weight Down"))
-            m_vVelocity -= new Vector3(10.0f, 10.0f, 0.0f) * Time.deltaTime;
+        GameObject weight = GameObject.Find("Weight");
+        GameObject board  = GameObject.Find("Board");
+
+        // first calculate the distance between the weight and the board
+        float d = weight.transform.position.y - board.transform.position.y; // assumes weight is always above board
+        d -= (weight.collider.bounds.size.y * 0.5f + board.collider.bounds.size.y * 0.5f);
+
+        // calculate time at which the collision between weight & board occurs
+        float t = Mathf.Sqrt(d / (0.5f * m_fGravity));
+
+        // calculate velocity of the weight at time t
+        float vW = m_fGravity * t;
+
+        // calculate board's velocity after collision
+        float vB = ((2.0f * weight.rigidbody.mass) / (weight.rigidbody.mass + board.rigidbody.mass)) * vW;
+
+        // finally, calculate the player's velocity after the board collides with it
+        return ((2.0f * board.rigidbody.mass) / (m_Player.rigidbody.mass + board.rigidbody.mass)) * vB;
     }
 
-    public void OnWedgeMoved()
+    private float _CalculateVelX()
     {
-        // Calculate the angle
-        if (Input.GetButton("Move Wedge Right"))
-            m_fAngle -= Time.deltaTime;
-        else if (Input.GetButton("Move Wedge Left"))
-            m_fAngle += Time.deltaTime;
+        GameObject board = GameObject.Find("Board");
+        GameObject wedge = GameObject.Find("Wedge");
+        GameObject ground= GameObject.Find("Ground");
+
+        // calculate distance of player from the center (hinge), this is the radius
+        float r = Mathf.Abs(m_Player.transform.position.x - wedge.transform.position.x);
+
+        // using right triangle to determine angle between board start position & 
+        //  when it contacts the ground:
+        // tan A = a/b
+        // theta = arctan(tan A)
+
+        // get the distance from the board to the ground
+        float a = Mathf.Abs((board.transform.position.y - board.collider.bounds.size.y * 0.5f) - ground.collider.bounds.max.y);
+
+        // get the distance from far end of board to center (hinge)
+        float b = board.collider.bounds.size.x * 0.5f;
+
+        // get the angle
+        float theta = Mathf.Atan(a / b);
+
+        // calculate distance the player will travel (before separation from board)
+        // this is known as Arc Length
+        float L = theta * r;
+
+        // calculate angular displacement
+        float ad = L / r;
+
+        // calculate the time at which separation will occur
+        float t = Mathf.Sqrt(a / (0.5f * m_fGravity));
+        
+        // calculate the angular velocity at time t
+        float w = ad / t;
+
+        return r * w;
     }
 }
