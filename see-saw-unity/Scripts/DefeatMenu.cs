0eacaad65914751864a6b4c14534de5ad7480c8f
allthingscandid@gmail.com <allthingscandid@gmail.com@8660b971-0f8b-ea4a-1521-bd2fb6e1095f>
2011-04-29 08:23:44 +0000

    updated input to work on iphone

--- see-saw-unity/Scripts/DefeatMenu.cs
+++ see-saw-unity/Scripts/DefeatMenu.cs
@@ -3,29 +3,51 @@ using System.Collections;
 
 public class DefeatMenu : MonoBehaviour
 {
-    GUIText m_Level;
-    GUIText m_MainMenu;
+    GameObject m_Level;
+    GameObject m_MainMenu;
 
     void Start()
     {
-        m_Level = GameObject.Find("ResetLevel").guiText;
-        m_MainMenu = GameObject.Find("MainMenu").guiText;
+        m_Level = GameObject.Find("BtnReset");
+        m_MainMenu = GameObject.Find("BtnMainMenu");
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonUp(0))
-        {
+#if UNITY_IPHONE
+		if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+		{
+			Touch touch = Input.GetTouch(0);
+        	Ray ray = Camera.main.ScreenPointToRay(new Vector3(touch.position.x, touch.position.y));
+			
             // TODO:: color text on hover:
-            if (m_Level.HitTest(Input.mousePosition))
+            if (m_Level.collider.bounds.IntersectRay(ray))
             {
                 Game.Instance.RedoCurrLevel();
             }
-            else if (m_MainMenu.HitTest(Input.mousePosition))
+            else if (m_MainMenu.collider.bounds.IntersectRay(ray))
+            {
+                // TODO:: perform any resetting necessary here:
+                Game.Instance.OnGotoMainMenu();
+            }
+		}
+#else
+        if (Input.GetMouseButtonUp(0))
+        {			
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			
+            // TODO:: color text on hover:
+            if (m_Level.collider.bounds.IntersectRay(ray))
+            {
+                Game.Instance.RedoCurrLevel();
+            }
+            else if (m_MainMenu.collider.bounds.IntersectRay(ray)
             {
                 // TODO:: perform any resetting necessary here:
                 Game.Instance.OnGotoMainMenu();
             }
         }
+#endif
     }
 }
+			
